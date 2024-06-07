34c44df0a28fac75151741c164b0aa03334411de
Mahdi Rahman <mahdi.rahman@gmail.com>
2024-06-07 15:26:42 +0300

    Update theme.js

--- static/js/theme.js
+++ static/js/theme.js
@@ -5,14 +5,18 @@ function setTheme(themeName) {
   themeStyle.href = `../static/css/theme_${themeName}.css`;
   localStorage.setItem("selectedTheme", themeName);
   const container = document.querySelector(".dark_matter_video");
+  const imgElement = document.querySelector(".img_container.happeo_img img");
+
   if (themeName === "dark") {
     container.classList.add("dark");
-    metaTheme.setAttribute('content', 'rgb(0, 0, 0)');
-    metaAppTheme.setAttribute('content', 'rgb(0, 0, 0)');
+    metaTheme.setAttribute("content", "rgb(0, 0, 0)");
+    metaAppTheme.setAttribute("content", "rgb(0, 0, 0)");
+    imgElement.src = "static/img/happeo_mobile_app_dark.png";
   } else {
     container.classList.remove("dark");
-    metaTheme.setAttribute('content', 'rgb(238, 234, 229)');
-    metaAppTheme.setAttribute('content', 'rgb(238, 234, 229)');
+    metaTheme.setAttribute("content", "rgb(238, 234, 229)");
+    metaAppTheme.setAttribute("content", "rgb(238, 234, 229)");
+    imgElement.src = "static/img/happeo_mobile_app.png";
   }
 }
 
@@ -22,24 +26,24 @@ function toggleTheme() {
   const container = document.querySelector(".dark_matter_video");
   const metaTheme = document.querySelector('meta[name="theme-color"]');
   const metaAppTheme = document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]');
-
-
+  const imgElement = document.querySelector(".img_container.happeo_img img");
 
   if (currentTheme === "light") {
     setTheme("dark");
     themeButton.setAttribute("dark", "");
     themeButton.removeAttribute("light");
     container.classList.add("dark");
-    metaTheme.setAttribute('content', 'rgb(0, 0, 0)');
-    metaAppTheme.setAttribute('content', 'rgb(0, 0, 0)');
-
+    metaTheme.setAttribute("content", "rgb(0, 0, 0)");
+    metaAppTheme.setAttribute("content", "rgb(0, 0, 0)");
+    imgElement.src = "static/img/happeo_mobile_app_dark.png";
   } else {
     setTheme("light");
     themeButton.setAttribute("light", "");
     themeButton.removeAttribute("dark");
     container.classList.remove("dark");
-    metaTheme.setAttribute('content', 'rgb(238, 234, 229)');
-    metaAppTheme.setAttribute('content', 'rgb(238, 234, 229)');
+    metaTheme.setAttribute("content", "rgb(238, 234, 229)");
+    metaAppTheme.setAttribute("content", "rgb(238, 234, 229)");
+    imgElement.src = "static/img/happeo_mobile_app.png";
   }
 }
 
@@ -50,9 +54,8 @@ document.addEventListener("DOMContentLoaded", () => {
   if (selectedTheme) {
     setTheme(selectedTheme);
     themeButton.setAttribute(selectedTheme, "");
- 
-   } else {
+  } else {
     setTheme("light");
     themeButton.setAttribute("light", "");
-   }
+  }
 });
